31d6ebe41ef5277d67aa9bdc817158780717d96b
aaron <aaron_merino@outlook.com>
2024-03-22 17:45:52 -0400

    shadows implementation

--- main.py
+++ main.py
@@ -20,40 +20,69 @@ class Scene:
        self.camera = camera
        self.lights = lights
 
+    def hit(self, ray, t0):
+      t1 = math.inf
+      hit_obj = None
+      normal = None
+
+      for o in self.objects:
+
+        hit, t, n = o.hit(ray, t0, t1)
+
+        if (hit):
+          t1 = t
+          hit_obj = o
+          normal = n
+      
+      return t1, hit_obj, normal
+    
+
     def render(self, canvas):
       for i in range(0, WIDTH):
         for j in range(0, HEIGHT):
           view_ray = self.camera.generate_view_ray(i, j)
 
-          t1 = math.inf
-          hit_obj = None
-          normal = None
+          # t1 = math.inf
+          # hit_obj = None
+          # normal = None
+          t1, hit_obj, normal = self.hit(view_ray, 0)
+
           pixel_color = None
 
-          for o in self.objects:
+          # for o in self.objects:
 
-            hit, t, n = o.hit(view_ray, 0, t1)
+          #   hit, t, n = o.hit(view_ray, 0, t1)
 
-            if (hit):
-              t1 = t
-              hit_obj = o
-              normal = n
+          #   if (hit):
+          #     t1 = t
+          #     hit_obj = o
+          #     normal = n
           
           if (hit_obj is not None):
-            ambient_light_intensity = Color(10, 45, 80).normalize()
+            ambient_light_intensity = Color(20, 85, 120).normalize()
 
             # default ambient color
             pixel_color = hit_obj.diffuse_color.normalize() * ambient_light_intensity
 
             # now go through each light source
             for light in self.lights:
+              # check if in shadow first:
+              hit_position = view_ray.getPoint(t1)
+              light_direction = (light.position - hit_position).normalize()
+
+              ray_light = Ray(hit_position, light_direction)
+
+              _, hit_shadow, _ = self.hit(ray_light, 0.01)
+              if (hit_shadow is not None):
+                continue
+
               diff_coef = hit_obj.diffuse_color.normalize()
 
               light_intensitiy = light.color.normalize()
 
-              hit_position = view_ray.getPoint(t1)
+              # hit_position = view_ray.getPoint(t1)
 
-              light_direction = (light.position - hit_position).normalize()
+              # light_direction = (light.position - hit_position).normalize()
 
               lambart = max(0, normal.dot(light_direction))
 
@@ -98,21 +127,27 @@ if __name__ == "__main__":
   scene_objects.append(Sphere(Color(255, 120, 255), Color(150, 150, 150), 80, Vec3(-12, 60, 20), 10))
   scene_objects.append(Sphere(Color(255, 120, 255), Color(150, 150, 150), 80, Vec3(12, 60, 20), 10))
   scene_objects.append(Sphere(Color(0, 0, 255), Color(150, 150, 150), 100, Vec3(-12, 80, 40), 10))
+  
 
   scene_objects.append(Triangle(Color(255, 120, 50), Color(150, 150, 150), 100, Vec3(0, 60, -30), Vec3(40, 60, -20), Vec3(10, 60, 10)))
   scene_objects.append(Triangle(Color(255, 120, 50), Color(150, 150, 150), 100, Vec3(0, 60, -30), Vec3(40, 60, -20), Vec3(10, 60, 10)))
   scene_objects.append(Triangle(Color(255, 120, 50), Color(150, 150, 150), 100, Vec3(0, 60, -30), Vec3(10, 40, -50), Vec3(40, 60, -20)))
 
-  scene_objects.append(Triangle(Color(50, 120, 250), Color(150, 150, 150), 100, Vec3(-50, 20, -80), Vec3(50, 20, -80), Vec3(-50, 200, -80)))
-  scene_objects.append(Triangle(Color(50, 120, 250), Color(150, 150, 150), 100, Vec3(50, 20, -80), Vec3(50, 200, -80), Vec3(-50, 200, -80)))
+  scene_objects.append(Triangle(Color(50, 120, 250), Color(150, 150, 150), 100, Vec3(-50, 20, -100), Vec3(50, 20, -100), Vec3(-50, 200, -100)))
+  scene_objects.append(Triangle(Color(50, 120, 250), Color(150, 150, 150), 100, Vec3(50, 20, -100), Vec3(50, 200, -100), Vec3(-50, 200, -100)))
 
-  scene_objects.append(Triangle(Color(50, 50, 50), Color(150, 150, 150), 100, Vec3(-50, 20, 80), Vec3(-50, 200, 80), Vec3(50, 20, 80)))
-  scene_objects.append(Triangle(Color(50, 50, 50), Color(150, 150, 150), 100, Vec3(50, 20, 80), Vec3(-50, 200, 80), Vec3(50, 200, 80)))
+  scene_objects.append(Triangle(Color(50, 50, 50), Color(150, 150, 150), 100, Vec3(-50, 20, 100), Vec3(-50, 200, 100), Vec3(50, 20, 100)))
+  scene_objects.append(Triangle(Color(50, 50, 50), Color(150, 150, 150), 100, Vec3(50, 20, 100), Vec3(-50, 200, 100), Vec3(50, 200, 100)))
 
   scene_lights = []
   scene_lights.append(PointLight(Vec3(300, 10, 500), Color(455/2, 150/2, 55/2)))
 
   scene_lights.append(PointLight(Vec3(-200, 10, -200), Color(100, 250, 120)))
+
+  # scene_objects.append(Sphere(Color(0, 0, 255), Color(150, 150, 150), 100, Vec3(150, 80, 60), 2))
+  scene_lights.append(PointLight(Vec3(150, 80, 70), Color(400, 400, 100)))
+
+  scene_lights.append(PointLight(Vec3(150, 10, 20), Color(200, 200, 100)))
 
 
   scene = Scene(scene_objects, scene_lights, camera)
